9a17ab09f9335d8c29f6f871800c7e288683990b
Vihrev Evgenij <evgeny.vihrev@gmail.com>
2020-05-17 06:41:40 +0300

    Not trying with static server

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/.well-known/pki-validation/'
 
 # Add these new lines
 STATICFILES_DIRS = (
